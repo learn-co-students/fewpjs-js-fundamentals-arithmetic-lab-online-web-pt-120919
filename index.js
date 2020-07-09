32351dc35861267cd59219226b432809d4a45925
rmricci121 <rmricci121@gmail.com>
2020-07-09 15:58:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 31
+
+let multiply = num1 + num2
+
+
+let random = Math.floor(Math.random() + 1)
+
+let mod = 17 % 3 +2
+
+let max = Math.max(1,20)
+
