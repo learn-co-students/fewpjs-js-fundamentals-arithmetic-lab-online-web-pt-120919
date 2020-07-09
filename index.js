af40a22398e0d4ee81288975cd09bb3fc5137618
MilanP5G <milan_parmar@hotmail.co.uk>
2020-07-09 11:00:35 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+multiply
+
+let random = Math.floor(Math.random() * 10) + 1
+
+random
+
+let mod = 24 % 5
+
+mod
+
+let max = Math.max(5, 10, 15, 20)
+
+max
