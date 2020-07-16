6f910f10cca9500940cf1572c957d1c896e63343
tnhubbar <taryn.johnson919@gmail.com>
2020-07-16 10:45:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 2 
+let num2 = 31
+let multiply = num1 * num2 
+
+let random = Math.floor(Math.random()*10) + 1;
+
+let mod = num2 % 9 
+
+let max = Math.max(1, 3, 20, 4, 5)
