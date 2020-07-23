91beb831225b9a71e75da71c2b211f9e05b6e669
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-07-22 23:44:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+
+
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() > 0)
+const mod = 15%11
+const max = Math.max(1,4,5,20) 
